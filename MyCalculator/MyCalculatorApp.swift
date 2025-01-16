f56a0c172d6113b11c25711954fae0b69ff3848f
Gnk255 <vadimgolubenko777@gmail.com>
2025-01-16 23:36:13 +0200

    Initial Commit

---
+++ MyCalculator/MyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorApp.swift
+//  MyCalculator
+//
+//  Created by Вадим on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
